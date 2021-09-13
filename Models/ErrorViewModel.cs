1448627de32c1f1d988803a02f88ac7a955cd93f
HANG LE THI <1821050908@student.humg.edu.vn>
2021-09-13 14:59:51 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoMVc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
